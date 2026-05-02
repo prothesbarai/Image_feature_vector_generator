d95b0752af92b20445a99676cdd773fafc286870
Prothes Barai <75435992+prothesbarai@users.noreply.github.com>
2026-05-02 08:59:51 +0600

    prothes

--- backend/main.py
+++ backend/main.py
@@ -16,11 +16,19 @@ app.add_middleware(
 )
 
 device = "cpu"
-#model, preprocess = clip.load("ViT-B/32", device=device)
-model, preprocess = clip.load("RN50", device=device)
+# model, preprocess = clip.load("ViT-B/32", device=device)
+model = None
+preprocess = None
+def load_model():
+    global model, preprocess
+    if model is None:
+        # Use lightweight model (IMPORTANT)
+        model, preprocess = clip.load("RN50", device=device)
+    return model, preprocess
 
 
 def get_embedding_from_image(image: Image.Image):
+    model, preprocess = load_model()
     image = image.convert("RGB")
 
     img1 = preprocess(image).unsqueeze(0).to(device)
@@ -37,6 +45,12 @@ def get_embedding_from_image(image: Image.Image):
 
     return feature_vector.squeeze().tolist()
 
+
+# Health check route
+@app.get("/")
+def home():
+    return {"status": "API is running 🚀"}
+    
 
 @app.post("/embed-image")
 async def embed_image(file: UploadFile = File(...)):
